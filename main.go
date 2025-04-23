76e6bfd6106a924d206e3b3eae57c1b0fc05a1ab
Dima Ustik <kukushonok.kukuhka@gmail.com>
2025-04-23 23:04:38 +0300

    css

--- main.go
+++ main.go
@@ -20,6 +20,7 @@ type WordData struct {
 func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*")
+	router.Static("/static", "./static")
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", nil)
 	})
